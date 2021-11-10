4d5466df4739ce27178c62701d34162d9d62a062
Enes AYDIN <89972814+enesaydiinn@users.noreply.github.com>
2021-11-10 10:19:40 +0300

    angular

---
+++ src/app/shared/payment-detail.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { PaymentDetail } from './payment-detail.model';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly baseURL='http://localhost:44862/api/PaymentDetail'
+  formData: PaymentDetail=new PaymentDetail();
+  list:PaymentDetail[];
+
+  putPaymentDetail(){
+    return this.http.put(`${this.baseURL}/${this.formData.paymentDetailId}`, this.formData);
+  }
+
+  postPaymentDetail(){
+    return this.http.post(this.baseURL,this.formData);
+   }
+
+  refreshList(){
+    this.http.get(this.baseURL)
+    .toPromise()
+    .then(res=>this.list=res as PaymentDetail[]);
+  }
+
+  deletePaymentDetail(id: number) {
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
+
+}
